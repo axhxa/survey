1e19926d7700fe205d7f70a0602a76c7a18611bd
axhxa <hynyhux@qq.com>
2020-03-02 13:04:59 +0800

    commit

--- app/src/main/java/mg/studio/android/survey/MainActivity.java
+++ app/src/main/java/mg/studio/android/survey/MainActivity.java
@@ -2,13 +2,27 @@ package mg.studio.android.survey;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.*;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    CheckBox c;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome);
+        c=findViewById(R.id.cbox);
+    }
+
+    public void onClickBtn(View view){
+        if(c.isChecked()) {
+            Intent intent = new Intent(this,question_one .class);
+            startActivity(intent);
+        }
+        else Toast.makeText(getApplicationContext(),"Need to agree the requests",Toast.LENGTH_LONG).show();
     }
 }
